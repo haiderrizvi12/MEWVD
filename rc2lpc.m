d7f5a166f195517be1ee6021f0249f9f5d2e6f32
haiderrizvi12 <102996082+haiderrizvi12@users.noreply.github.com>
2022-04-06 00:31:45 +0500

    Update rc2lpc.m

--- rc2lpc.m
+++ rc2lpc.m
@@ -1,17 +1,16 @@
-function A = rc2lpc(K)
 
+function A = rc2lpc(K)
 % RC2LPC Convert Reflection Cofficients to Linear Prediction Coefficients.
 
 
-p = length(K); % Order of linear prediction
-        a1 = K(1); % a1(1) = k1
-        for i = 2:p
-            a = a1;
+        n = 1; 
+        for i = 2:length(K) %order
+            a = n;
             for j = 1:(i-1)
-                a1(j) = a(j) + K(i)*a(i-j);
+                n(j) = a(j) + K(i)*a(i-j);
             end
-            a1(i) = K(i);
+            n(i) = K(i);
         end
-        A = [1, a1];
+        A = [1, n];
     
 end
